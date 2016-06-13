11deb667e7ef61da32118378d4b7ba87751309cb
Pedro Ayala <peayala@cisco.com>
2016-06-13 10:56:13 +0100

    Add list of users

--- data/schema.ts
+++ data/schema.ts
@@ -1,7 +1,23 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLID } from 'graphql';
 import { Db } from 'mongodb';
 
 export let Schema = (db: Db) => {
+
+  let userType = new GraphQLObjectType({
+    name: 'user',
+    fields: () => ({
+      _id: { type: GraphQLString },
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      surname: { type: GraphQLString },
+      email: { type: GraphQLString },
+      seeds: {
+        type: new GraphQLList(seedType),
+        resolve: (parent, args) =>
+          db.collection('seeds').find({ user_id: parent.id }).toArray()
+      }
+    })
+  });
 
   let seedType = new GraphQLObjectType({
     name: 'seed',
@@ -9,10 +25,14 @@ export let Schema = (db: Db) => {
       _id: { type: GraphQLString },
       name: { type: GraphQLString },
       description: { type: GraphQLString },
+      user: {
+        type: userType,
+        resolve: (parent, args) =>
+          db.collection('users').find({ id: parent.user_id }).limit(1).next()
+      },
       location: { type: GraphQLString }
     })
   });
-
 
   let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -21,6 +41,10 @@ export let Schema = (db: Db) => {
         seeds: {
           type: new GraphQLList(seedType),
           resolve: () => db.collection('seeds').find({}).toArray()
+        },
+        users: {
+          type: new GraphQLList(userType),
+          resolve: () => db.collection('users').find({}).toArray()
         }
       })
     })
